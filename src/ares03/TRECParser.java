c8eb5b8e733bc7f605bfcf294adf99582688cf9d
Kostas Gottis <phoinikaskg@gmail.com>
2014-06-24 02:33:51 +0300

    Update TRECParser.java

--- src/ares03/TRECParser.java
+++ src/ares03/TRECParser.java
@@ -2,9 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ares03;
-
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -18,57 +16,58 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Klasi pou ilopoiei oles tis leitourgies parsing pou hreiazetai to project.
- * 
+ * A class that implements parsing methods.
+ *
  * @author Terminal
  */
 public class TRECParser {
 
-    //identifiers
+    // identifiers
     private String id = ".I";
     private String content = ".W";
     private String title = ".T";
     private String ignoreA = ".A";
     private String ignoreB = ".B";
-    private int maxTF ;  //Briskei tin megisti syxnotita emfanisis kapias leksis  sto ka8e doc
+    private int maxTF;  //Briskei tin megisti syxnotita emfanisis kapias leksis  sto ka8e doc
     private String stopwordsFile = "stopwords.txt";
-    
+
     /**
-     * 
+     *
      * @return
      */
     public ArrayList<Document> readDocs(String inputFileName) {
         ArrayList<Document> docList = new ArrayList<Document>();
         String words = "";
         String titlewords = "";
-        int docID = 0;       
-        int mode = 1;        // 1 gia diavasma word, 2 gia diavasma title, 3 gia diavasma ignore
+        int docID = 0;
+        int mode = 1;        // 1 read word, 2 read title, 3 read ignore
         int firstDoc = 0;
 
 
-        if(inputFileName.contains(".pdf") || inputFileName.contains(".rtf")){ //an to arheio einai pdf i rtf
+        if (inputFileName.contains(".pdf") || inputFileName.contains(".rtf")) { //an to arheio einai pdf i rtf
             String text = "";
-            // eleghos tou format tou arheiou
-            if(inputFileName.contains(".pdf")){
+            // chack file format
+            if (inputFileName.contains(".pdf")) {
                 PDFTextParser pdfparser = new PDFTextParser();
                 text = pdfparser.pdftoText(inputFileName);
-            }
-            else if(inputFileName.contains(".rtf")){
+            } else if (inputFileName.contains(".rtf")) {
                 RTFTextParser rtfparser = new RTFTextParser();
                 text = rtfparser.rtftoText(inputFileName);
             }
-            
+
             StringTokenizer in;
 
-            in= new StringTokenizer(text," \n");
+            in = new StringTokenizer(text, " \n");
 
-            while(in.hasMoreTokens()){
+            while (in.hasMoreTokens()) {
                 String token = in.nextToken();
 
-                if(token.equals(id)){
+                if (token.equals(id)) {
 
-                    if(firstDoc == 1){
+                    if (firstDoc == 1) {
+
                         Document doc = new Document(docID, titlewords, words);
+
                         docList.add(doc);
                         doc.toString();
                         words = "";
@@ -78,30 +77,22 @@ public class TRECParser {
                     firstDoc = 1;
                     docID = Integer.parseInt(in.nextToken());
 
-                    }
-                    else if(token.equals(content)){
-                        mode = 1;
-                    }
-                    else if(token.equals(title)){
-                        mode = 2;
-                    }
-                    else if(token.equals(ignoreA) || token.equals(ignoreB)){
-                        mode = 3;
-                    }
-                    else{
-                        if(mode == 1){
-                        //words.add(token);
-                            words += token + " ";
-                        }
-                        else if(mode == 2){
-                        //titlewords.add(token);
-                            titlewords += token + " ";
-                    }
-                        else if(mode == 3){
+                } else if (token.equals(content)) {
+                    mode = 1;
+                } else if (token.equals(title)) {
+                    mode = 2;
+                } else if (token.equals(ignoreA) || token.equals(ignoreB)) {
+                    mode = 3;
+                } else {
+                    if (mode == 1) {
+                        words += token + " ";
+                    } else if (mode == 2) {
+                        titlewords += token + " ";
+                    } else if (mode == 3) {
                         continue;
                     }
                 }
-             }
+            }
 
 
 
@@ -110,55 +101,49 @@ public class TRECParser {
             docList.add(doc);
             doc.toString();
 
-        }
-        else{ //an to arheio einai .txt
+        } else { // if txt
             try {
 
-            BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-            StringTokenizer in;
+                BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+                StringTokenizer in;
 
-            String line = "";
-            while((line = reader.readLine()) != null){
-                in= new StringTokenizer(line," \n");
+                String line = "";
+                while ((line = reader.readLine()) != null) {
 
-                while(in.hasMoreTokens()){
-                    String token = in.nextToken();
+                    in = new StringTokenizer(line, " \n");
 
-                    if(token.equals(id)){
+                    while (in.hasMoreTokens()) {
+                        String token = in.nextToken();
 
-                        if(firstDoc == 1){
-                            Document doc = new Document(docID, titlewords, words);
-                            docList.add(doc);
-                            doc.toString();
-                            words = "";
-                            titlewords = "";
-                        }
+                        if (token.equals(id)) {
 
-                        firstDoc = 1;
-                        docID = Integer.parseInt(in.nextToken());
-
-                        }
-                        else if(token.equals(content)){
-                            mode = 1;
-                        }
-                        else if(token.equals(title)){
-                            mode = 2;
-                        }
-                        else if(token.equals(ignoreA) || token.equals(ignoreB)){
-                            mode = 3;
-                        }
-                        else{
-                            if(mode == 1){
-                                words += token + " ";
+                            if (firstDoc == 1) {
+                                Document doc = new Document(docID, titlewords, words);
+                                docList.add(doc);
+                                doc.toString();
+                                words = "";
+                                titlewords = "";
                             }
-                            else if(mode == 2){
+
+                            firstDoc = 1;
+                            docID = Integer.parseInt(in.nextToken());
+
+                        } else if (token.equals(content)) {
+                            mode = 1;
+                        } else if (token.equals(title)) {
+                            mode = 2;
+                        } else if (token.equals(ignoreA) || token.equals(ignoreB)) {
+                            mode = 3;
+                        } else {
+                            if (mode == 1) {
+                                words += token + " ";
+                            } else if (mode == 2) {
                                 titlewords += token + " ";
-                        }
-                            else if(mode == 3){
-                            continue;
+                            } else if (mode == 3) {
+                                continue;
+                            }
                         }
                     }
-                 }
 
                 }
 
@@ -176,46 +161,45 @@ public class TRECParser {
                 Logger.getLogger(TRECParser.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
 
         return docList;
     }
 
-
     /**
-     * epistrefei ena hasmap pou periexei ta sxetika eggrafa gia ka8e query, otan oi sillogh einai anaforas.
+     * Returns a hashmap with query similar documents.
      *
-     * @param inputFileName to arxeio me ta sxetika eggrafa gia ka8e query
-     * @return  ena hasmap pou periexei to id tou query kai mia lista me ta sxetika eggrafa gia ka8e query
+     * @param inputFileName a file with query similar docs
+     * @return a hashmap with query similar docs for every query id
      */
-    public HashMap<Integer,ArrayList<Integer>> readRelevant(String inputFileName){
-        HashMap<Integer,ArrayList<Integer>> relevantDocs = new HashMap<Integer,ArrayList<Integer>>();
-        
+    public HashMap<Integer, ArrayList<Integer>> readRelevant(String inputFileName) {
+        HashMap<Integer, ArrayList<Integer>> relevantDocs = new HashMap<Integer, ArrayList<Integer>>();
+
         ArrayList<Integer> relevantDocList = null;
-                       
+
         try {
-  
+
             BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
             StringTokenizer in;
 
             String line = "";
             int check = 0;
-            while((line = reader.readLine()) != null){
-                in= new StringTokenizer(line," \n");
+            while ((line = reader.readLine()) != null) {
 
+                in = new StringTokenizer(line, " \n");
 
-                while(in.hasMoreTokens()){
+                while (in.hasMoreTokens()) {
                     String token = in.nextToken();
-                                      
-                    if(token.equals("0")){
+
+                    if (token.equals("0")) {
                         continue;
                     }
-                    if (relevantDocs.containsKey(Integer.parseInt(token))){
+                    if (relevantDocs.containsKey(Integer.parseInt(token))) {
                         relevantDocs.get(Integer.parseInt(token)).add(Integer.parseInt(in.nextToken()));
                         continue;
                     }
 
-                    if (!relevantDocs.containsKey(Integer.parseInt(token))){
+                    if (!relevantDocs.containsKey(Integer.parseInt(token))) {
 
                         relevantDocList = new ArrayList<Integer>();
                         relevantDocs.put(Integer.parseInt(token), relevantDocList);
@@ -224,148 +208,148 @@ public class TRECParser {
                 }
             }
             reader.close();
-           
+
 
         } catch (FileNotFoundException ex) {
             Logger.getLogger(TRECParser.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(TRECParser.class.getName()).log(Level.SEVERE, null, ex);
-        }      
+        }
         return relevantDocs;
 
     }
 
-
-     /**
-     * epistrefei hashmap me tous ksexwristous orous tou eggrafou, episis gia ka8e eggrafo dinei to maxtf kai to size tou
-     * 
+    /**
+     * Returns a hashmap with the unique doc terms. Also calculates max term frequency and size.
+     *
      * @return terms
      */
-    public HashMap getTerms(Document d){
+    public HashMap getTerms(Document d) {
 
-        HashMap<String,Integer> terms = new HashMap<String,Integer>();
+        HashMap<String, Integer> terms = new HashMap<String, Integer>();
         StringTokenizer stok;
         String token;
         int tf = maxTF = 0;
-        //hash set gia to stopwords
+        // hash set for stopwords
         HashSet<String> stopwords = this.stopWords(stopwordsFile);
 
 
-        //an to periehomeno tou eggrafou den einai keno
-        if(!d.getContent().equals("")){
-            //tokenize
+        // if doc content is not empty
+        if (!d.getContent().equals("")) {
+            // tokenize
             stok = new StringTokenizer(d.getContent(), " ,.\n");
-            while(stok.hasMoreTokens()){
+            while (stok.hasMoreTokens()) {
                 token = stok.nextToken();
 
-                //an to token einai stopword sinexise me to epomeno
-                if(stopwords.contains(token)){
+                // if doc is a stopword continue with the next one
+                if (stopwords.contains(token)) {
                     continue;
                 }
 
-                if(terms.containsKey(token)){
+                if (terms.containsKey(token)) {
                     tf = terms.get(token);
                     tf++;
                     terms.put(token, tf);
-                    if(tf>maxTF) maxTF =tf;
-                }
-                else{
+                    if (tf > maxTF) {
+                        maxTF = tf;
+                    }
+                } else {
                     terms.put(token, 1);
                 }
-                
-                
+
+
             }
         }
 
-        //an o titlos tou eggrafou den einai kenos
-        if(!d.getTitle().equals("")){
+        // if title is not empty
+        if (!d.getTitle().equals("")) {
             //tokenize
             stok = new StringTokenizer(d.getTitle(), " ,.\n");
-            while(stok.hasMoreTokens()){
+            while (stok.hasMoreTokens()) {
                 token = stok.nextToken();
 
-                if(terms.containsKey(token)){
+                if (terms.containsKey(token)) {
                     tf = terms.get(token);
                     tf++;
                     terms.put(token, tf);
-                    if(tf>maxTF) maxTF =tf;
-                }
-                else{
+                    if (tf > maxTF) {
+                        maxTF = tf;
+                    }
+                } else {
                     terms.put(token, 1);
                 }
             }
         }
-         //gia ka8e document briskei to maxTF kai to size tou(ari8mos ksexwristwn orwn)
-         d.setMaxTf(maxTF);
-         d.setSize(terms.size());
+        // set max term frequency and size
+        d.setMaxTf(maxTF);
+        d.setSize(terms.size());
 
-         return terms;
+        return terms;
     }
 
-    public int getMaxTF(){
+    public int getMaxTF() {
         return maxTF;
     }
 
-     /**
-     *gia ta queries
+    /**
+     * For queries.
      *
      * @return terms
      */
-    public HashMap getTerms(String str){
+    public HashMap getTerms(String str) {
 
-        HashMap<String,Integer> terms = new HashMap<String,Integer>();
+        HashMap<String, Integer> terms = new HashMap<String, Integer>();
         StringTokenizer stok;
         String token;
         int tf = 0;
-        //hash set gia to stopwords
+        // hash set for stopwords
         HashSet<String> stopwords = this.stopWords(stopwordsFile);
 
 
-        //an to periehomeno tou eggrafou den einai keno
-        if(!str.equals("")){
+        // if doc content is not empty
+        if (!str.equals("")) {
             //tokenize
             stok = new StringTokenizer(str, " ,.\n");
-            while(stok.hasMoreTokens()){
+            while (stok.hasMoreTokens()) {
                 token = stok.nextToken();
 
-                //an to token einai stopword sinexise me to epomeno
-                if(stopwords.contains(token)){
+                // if doc is a stopword continue with the next one
+                if (stopwords.contains(token)) {
                     continue;
                 }
 
-                if(terms.containsKey(token)){
+                if (terms.containsKey(token)) {
                     tf = terms.get(token);
                     tf++;
                     terms.put(token, tf);
-                }
-                else{
+                } else {
                     terms.put(token, 1);
                 }
             }
         }
 
-         return terms;
+        return terms;
     }
 
     /**
-     * dimiourgei ena HashSet me stopwords.
+     * Creates a hashset with stopwords.
      *
-     * @param stopwordsFileName to path kai to onoma tou txt arxeiou pou periexei ta stopwords
-     * @return  ena HashSet me ta stopwords
+     * @param stopwordsFileName filename and path containing stopwords
+     * @return a hashset with stopwords
      */
-    public HashSet stopWords(String stopwordsFileName){
-        HashSet <String> stopwords = new HashSet<String>();
+    public HashSet stopWords(String stopwordsFileName) {
+        HashSet<String> stopwords = new HashSet<String>();
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(stopwordsFileName));
             StringTokenizer in;
 
             String line = "";
-            while((line = reader.readLine()) != null){
-                
-                in= new StringTokenizer(line," \n");
-                
-                while(in.hasMoreTokens()){
+            while ((line = reader.readLine()) != null) {
+
+                in = new StringTokenizer(line, " \n");
+
+                while (in.hasMoreTokens()) {
                     String token = in.nextToken();
                     stopwords.add(token);
                 }
